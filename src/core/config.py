a5f09d9214355e453532b188c2e2f68aaa8321e5
SadLaboka <jiutbuh@gmail.com>
2026-05-10 17:29:39 +0300

    update Settings for working with the database

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,8 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import importlib.metadata
 
-_PACKAGE_NAME = "DocuMind"
+_PACKAGE_NAME = "docmind"
 
 try:
     APP_NAME = _PACKAGE_NAME
@@ -10,11 +11,35 @@ except importlib.metadata.PackageNotFoundError:
     APP_NAME = "dev"
     APP_VERSION = "0.0.0"
 
+ENV_FILE = ".env"
+
+class SettingsBase(BaseSettings):
+    model_config = SettingsConfigDict(env_file=ENV_FILE, extra="ignore")
+
+
+class DatabaseSettings(SettingsBase):
+    model_config = SettingsConfigDict(env_file=ENV_FILE, env_prefix="DB_")
+
+    user: str
+    password: str
+    name: str
+    host: str
+    port: int
+
+    @property
+    def url(self) -> str:
+        """Returns a ready URL for connecting to PostgreSQL."""
+        return (
+            f"postgresql+asyncpg://{self.user}:{self.password}"
+            f"@{self.host}:{self.port}/{self.name}"
+        )
+
 
 class Settings(BaseSettings):
-    title_name: str = APP_NAME
-    title_version: str = APP_VERSION
+    db: DatabaseSettings = Field(default_factory=DatabaseSettings)
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    app_name: str = APP_NAME
+    app_version: str = APP_VERSION
+
 
 settings = Settings()
