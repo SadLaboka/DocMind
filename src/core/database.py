f4d54cf2545e0fc3485f7f47cb8926309b36e874
SadLaboka <jiutbuh@gmail.com>
2026-05-13 22:57:20 +0300

    refactoring

--- src/core/database.py
+++ src/core/database.py
@@ -2,7 +2,7 @@ from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from core.config import settings
+from src.core.config import settings
 
 engine = create_async_engine(settings.db.url, future=True, echo=True)
 
