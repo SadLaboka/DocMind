ceb2d116b26a74cbc6ee6588a3d8a1f050eb0f6a
SadLaboka <jiutbuh@gmail.com>
2026-05-16 23:56:37 +0300

    update user-model and generate new migration

--- src/models/users.py
+++ src/models/users.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Boolean, String
+import datetime
+
+from sqlalchemy import Boolean, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.base import Base
@@ -7,9 +9,11 @@ from src.models.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    login: Mapped[str] = mapped_column(String(25), unique=True)
-    password_hash: Mapped[str]
-    email: Mapped[str] = mapped_column(String(40), unique=True)
+    login: Mapped[str] = mapped_column(String(25), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(128), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    failed_login_attempts: Mapped[int] = mapped_column(Integer, default=0)
+    last_login: Mapped[datetime.datetime] = mapped_column(nullable=True, default=None)
