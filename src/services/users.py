d1431f7e59d5baf93888f8ea054af6da8001459d
SadLaboka <jiutbuh@gmail.com>
2026-05-17 22:44:04 +0300

    implement method for register in UserService

--- src/services/users.py
+++ src/services/users.py
@@ -1,6 +1,25 @@
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+
+from core.security import get_password_hash
+from schemas.users import UserRegisterResponse, UserRegisterRequest, UserData
 from src.repositories.users import UserRepository
 from src.services.base import BaseService
 
 
 class UserService(BaseService[UserRepository]):
-    pass
+    async def register(self, data: UserRegisterRequest) -> UserRegisterResponse:
+        password_hash = get_password_hash(data.password)
+
+        used_data = UserData(
+            login=data.login,
+            email=data.email,
+            password_hash=password_hash
+        )
+
+        try:
+            created_user = await self.repository.create_user(used_data)
+        except IntegrityError:
+            raise HTTPException(status_code=409, detail="Login or email already exists")
+
+        return UserRegisterResponse.model_validate(created_user)
