7b53ecdb16e2b5925710eda4e032781b6deb6ac4
SadLaboka <jiutbuh@gmail.com>
2026-05-18 23:00:04 +0300

    implement create_user method

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,7 +1,18 @@
-from schemas.users import UserData, UserRegisterResponse
+from src.schemas.users import UserData
 from src.repositories.base import BaseRepository
+from src.models.users import User
 
 
 class UserRepository(BaseRepository):
-    async def create_user(self, data: UserData):
-        pass
+    async def create_user(self, data: UserData) -> User:
+
+        user = User(
+            login=data.login,
+            email=data.email,
+            password_hash=data.password_hash
+        )
+
+        self.session.add(user)
+        await self.session.flush()
+
+        return user
