d86c0e8c5a9dcc4d931da0268b2296659b039b13
SadLaboka <jiutbuh@gmail.com>
2026-05-19 13:34:46 +0300

    implement create-document method in documents-repository

--- src/repositories/documents.py
+++ src/repositories/documents.py
@@ -1,5 +1,13 @@
-from repositories.base import BaseRepository
+from src.repositories.base import BaseRepository
+from src.schemas.documents import DocumentData
+from src.models.documents import Document
 
 
 class DocumentRepository(BaseRepository):
-    pass
+    async def create_document(self, data: DocumentData) -> Document:
+        document = Document(**data.model_dump())
+
+        self.session.add(document)
+        await self.session.flush()
+
+        return document
