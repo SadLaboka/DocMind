e82b5df8dd37b91a389e75346d3004445794b487
SadLaboka <jiutbuh@gmail.com>
2026-05-20 23:55:05 +0300

    implement method for getting user by login

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -2,6 +2,8 @@ from src.schemas.users import UserData
 from src.repositories.base import BaseRepository
 from src.models.users import User
 
+from sqlalchemy import select
+
 
 class UserRepository(BaseRepository):
     async def create_user(self, data: UserData) -> User:
@@ -14,5 +16,11 @@ class UserRepository(BaseRepository):
 
         self.session.add(user)
         await self.session.flush()
+
+        return user
+
+    async def get_user_by_username(self, username: str) -> User | None:
+        result = await self.session.execute(select(User).where(User.login == username))
+        user = result.scalar_one_or_none()
 
         return user
