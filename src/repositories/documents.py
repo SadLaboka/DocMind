dba04ab2739b3a573590fdfcafe6fcb1f968b2d9
SadLaboka <jiutbuh@gmail.com>
2026-05-22 23:54:46 +0300

    add methods to execute documents from db

--- src/repositories/documents.py
+++ src/repositories/documents.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from src.models.documents import Document
 from src.repositories.base import BaseRepository
 from src.schemas.documents import DocumentData
@@ -11,3 +13,20 @@ class DocumentRepository(BaseRepository):
         await self.session.flush()
 
         return document
+
+    async def get_document_by_id(self, document_id: int) -> Document | None:
+        result = await self.session.execute(select(Document).where(Document.id == document_id))
+        document = result.scalar_one_or_none()
+        return document
+
+    async def get_document_by_id_and_user(self, document_id: int, user_id: int) -> Document | None:
+        result = await (self.session
+                        .execute(select(Document)
+                        .where(Document.id == document_id, Document.user_id == user_id)))
+        document = result.scalar_one_or_none()
+        return document
+
+    async def get_documents_list(self, user_id: int) -> list[Document]:
+        result = await self.session.execute(select(Document).where(Document.user_id == user_id))
+        documents = result.scalars().all()
+        return list(documents)
