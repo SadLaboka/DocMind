2f27f54a1bb4d180437a41ec7f146ecf097256a5
SadLaboka <jiutbuh@gmail.com>
2026-05-24 11:55:29 +0300

    fixes

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,8 +1,15 @@
+from starlette.status import (
+HTTP_401_UNAUTHORIZED,
+HTTP_404_NOT_FOUND, HTTP_409_CONFLICT,
+HTTP_500_INTERNAL_SERVER_ERROR
+)
+
+
 class AppBaseError(Exception):
-    status_code: int = 500
+    status_code: int = HTTP_500_INTERNAL_SERVER_ERROR
     error_code: str = "Internal_error"
     message: str = "Internal server error"
-    log_context: dict | None = None
+    log_context: dict
 
     def __init__(self, error_code: str | None = None, message: str | None = None, log_context: dict | None = None):
         self.error_code = error_code if error_code else type(self).error_code
@@ -12,18 +19,18 @@ class AppBaseError(Exception):
 
 
 class AuthenticationError(AppBaseError):
-    status_code = 401
+    status_code = HTTP_401_UNAUTHORIZED
     error_code =  "unauthorized"
     message = "Invalid credentials"
 
 
 class ResourceNotFoundError(AppBaseError):
-    status_code = 404
+    status_code = HTTP_404_NOT_FOUND
     error_code = "not_found"
     message = "Resource not found"
 
 
 class ConflictError(AppBaseError):
-    status_code = 409
+    status_code = HTTP_409_CONFLICT
     error_code = "conflict"
     message = "Resource already exists"
