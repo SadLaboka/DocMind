f10f5896635cbfc87ebfd3ba930984a546ccf80f
SadLaboka <jiutbuh@gmail.com>
2026-06-09 00:11:17 +0300

    add separated session-factory for celery

--- src/core/database.py
+++ src/core/database.py
@@ -1,6 +1,7 @@
 from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.pool import NullPool
 
 from src.core.config import settings
 
@@ -8,6 +9,13 @@ engine = create_async_engine(settings.db.url, future=True, echo=settings.logs.de
 
 async_session_factory = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
+celery_engine = create_async_engine(
+    settings.db.url,
+    future=True,
+    echo=settings.logs.dev,
+    poolclass=NullPool
+)
+celery_session_factory = async_sessionmaker(celery_engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_session() -> AsyncGenerator[AsyncSession]:
 
