8ecb51fcd89370b3578202e43e3d3eaa3baef29f
SadLaboka <jiutbuh@gmail.com>
2026-06-17 11:52:30 +0300

    add mongo-repository to DI in services

--- src/DependencyInjection/documents.py
+++ src/DependencyInjection/documents.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.repositories.mongo_documents import MongoDocumentRepository
 from src.core.database import get_session
 from src.repositories.documents import DocumentRepository
 from src.services.documents import DocumentService
@@ -11,9 +12,19 @@ def get_document_repository(session: AsyncSession = Depends(get_session)) -> Doc
     return DocumentRepository(session)
 
 
-def get_upload_service(repository: DocumentRepository = Depends(get_document_repository)) -> UploadService:
-    return UploadService(repository)
+def get_mongo_document_repository() -> MongoDocumentRepository:
+    return MongoDocumentRepository()
 
 
-def get_document_service(repository: DocumentRepository = Depends(get_document_repository)) -> DocumentService:
-    return DocumentService(repository)
+def get_upload_service(
+        repository: DocumentRepository = Depends(get_document_repository),
+        mongo_repository: MongoDocumentRepository = Depends(get_mongo_document_repository)
+) -> UploadService:
+    return UploadService(repository, mongo_repository)
+
+
+def get_document_service(
+        repository: DocumentRepository = Depends(get_document_repository),
+        mongo_repository: MongoDocumentRepository = Depends(get_mongo_document_repository)
+) -> DocumentService:
+    return DocumentService(repository, mongo_repository)
--- src/services/base.py
+++ src/services/base.py
@@ -1,10 +1,12 @@
 from typing import TypeVar
 
+from src.repositories.mongo_documents import MongoDocumentRepository
 from src.repositories.base import BaseRepository
 
 T = TypeVar("T", bound=BaseRepository)
 
 
 class BaseService[T]:
-    def __init__(self, repository: T):
+    def __init__(self, repository: T, mongo_repository: MongoDocumentRepository):
         self.repository = repository
+        self.mongo_repository = mongo_repository
