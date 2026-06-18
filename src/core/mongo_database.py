739c60cf8a28e443880cc585251832e2a16a5cd3
SadLaboka <jiutbuh@gmail.com>
2026-06-18 17:04:46 +0300

    add mongo initiator for celery-worker

--- src/core/mongo_database.py
+++ src/core/mongo_database.py
@@ -9,6 +9,7 @@ from src.models.mongo_documents import MongoDocument
 
 @asynccontextmanager
 async def init_mongo_db(app: FastAPI):
+    """Init mongo for main app"""
     client = AsyncMongoClient(settings.mongo.url)
     database = client[settings.mongo.name]
 
@@ -18,3 +19,13 @@ async def init_mongo_db(app: FastAPI):
     )
 
     yield
+
+
+async def init_mongo_for_worker() -> None:
+    """Init mongo for celery worker"""
+    client = AsyncMongoClient(settings.mongo.url)
+    database = client[settings.mongo.name]
+    await init_beanie(
+        database=database,
+        document_models=[MongoDocument],
+    )
