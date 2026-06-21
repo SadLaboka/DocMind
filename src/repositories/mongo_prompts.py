a488b747e63075dc3494e6a64c61b55d81716c79
SadLaboka <jiutbuh@gmail.com>
2026-06-21 13:11:32 +0300

    implement repository for prompts

---
+++ src/repositories/mongo_prompts.py
@@ -0,0 +1,32 @@
+from src.models.mongo_prompts import Prompt
+from src.core.exceptions import ConflictError
+
+
+class MongoPromptsRepository:
+    async def get_active_prompt(self, prompt_type: str) -> Prompt | None:
+        return await Prompt.find_one(Prompt.prompt_type == prompt_type, Prompt.is_active == True)
+
+    async def get_prompt_by_version(self, prompt_version: str) -> Prompt | None:
+        return await Prompt.find_one(Prompt.version == prompt_version)
+
+    async def create_prompt(self, version: str, prompt_type: str, content: str) -> Prompt | None:
+        prompt_with_version = await self.get_prompt_by_version(version)
+
+        if prompt_with_version:
+            raise ConflictError()
+
+        active_prompt = await self.get_active_prompt(prompt_type)
+        if active_prompt:
+            active_prompt.is_active = False
+            await active_prompt.save()
+
+        new_prompt = Prompt(
+            version=version,
+            prompt_type=prompt_type,
+            content=content,
+            is_active=True,
+        )
+
+        await new_prompt.insert()
+
+        return new_prompt
