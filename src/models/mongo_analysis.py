02484e2de8cf9015c145266ebc36644f17c0d99a
SadLaboka <jiutbuh@gmail.com>
2026-08-23 21:55:27 +0300

    add model for analysis

---
+++ src/models/mongo_analysis.py
@@ -0,0 +1,19 @@
+import pymongo
+from beanie import BeanieObjectId
+
+from src.core.enums import LLMProvider
+from src.models.mongo_base import BaseDocument
+
+
+class DocumentAnalysis(BaseDocument):
+    id: BeanieObjectId
+    document_id: int  #type: ignore
+    request_id: str
+    provider: LLMProvider
+    prompt_version: str | None = None
+    result: str | None = None
+
+    class Settings:
+        indexes = [
+            pymongo.IndexModel([("document_id", pymongo.ASCENDING), ("request_id", pymongo.ASCENDING)] , unique=True),
+        ]
