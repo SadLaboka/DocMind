137677d024d6d1024d65ca5c97fc2b705a4d99b6
SadLaboka <jiutbuh@gmail.com>
2026-08-30 18:37:52 +0300

    add service for get or create analysis with duplicatekey exception handler

---
+++ src/services/analysis.py
@@ -0,0 +1,27 @@
+from pymongo.errors import DuplicateKeyError
+
+from src.models.mongo_analysis import DocumentAnalysis
+from src.repositories.mongo_analyses import MongoAnalysisRepository
+from src.core.enums import LLMProvider
+
+
+class AnalysisService:
+    def __init__(self, analysis_repository: MongoAnalysisRepository) -> None:
+        self.repository = analysis_repository
+
+    async def get_or_create_analysis(
+            self,
+            document_id: int,
+            request_id: str,
+            provider: LLMProvider,
+    ) -> DocumentAnalysis:
+
+        analysis = await self.repository.get_analysis_by_document_and_request(document_id, request_id)
+
+        if not analysis:
+            try:
+                analysis = await self.repository.create_analysis(document_id, request_id, provider)
+            except DuplicateKeyError:
+                analysis = await self.repository.get_analysis_by_document_and_request(document_id, request_id)
+
+        return analysis
