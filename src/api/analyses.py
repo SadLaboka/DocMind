15cd23f22a512a169aa8dc7b2c4cb1d2af6cfc4a
SadLaboka <jiutbuh@gmail.com>
2026-09-18 23:23:27 +0300

    fix analyses route

--- src/api/analyses.py
+++ src/api/analyses.py
@@ -11,7 +11,7 @@ from src.schemas.users import User
 from src.services.analysis import AnalysisService
 from src.services.documents import DocumentService
 
-router = APIRouter(prefix="/analysis{document_id}", tags=["analysis"])
+router = APIRouter(prefix="/analysis/{document_id}", tags=["analysis"])
 http_bearer = HTTPBearer(auto_error=False)
 
 
